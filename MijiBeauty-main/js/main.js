67e013eea02dda6e1fe43fb7fdccda78fc70cf39
AllanxSF <al.salinast@duocuc.cl>
2025-09-06 14:14:07 -0400

    Validacion Registro

--- MijiBeauty-main/js/main.js
+++ MijiBeauty-main/js/main.js
@@ -1,12 +1,15 @@
 
-let nombres = [];
-let correos = [];
-let contraseñas = [];
+let nombresR = [];
+let correosR = [];
+let contraseñasR = [];
+let contraseñas2R = [];
 
-function validar(e) {
+function validar_registro() {
     let correo = document.getElementById("correo");
     let nombre = document.getElementById("nombre");
     let contraseña = document.getElementById("contraseña");
+    let contraseña2 = document.getElementById("contraseña2");
+
     if (nombre.value.trim() == "") {
         alert("ingrese un nombre");
     }
@@ -16,9 +19,29 @@ function validar(e) {
     } else if (contraseña.value.trim() == "") {
         alert("ingrese una contraseña");
 
+    } else if (contraseña2.value.trim() == "") {
+        alert("ingrese una contraseña");
+
+    } else if (contraseña2.value.trim() != contraseña.value.trim()) {
+        alert("Las contraseñas no coinciden");
+
     } else {
-        nombres.push(nombre.value);
-        correos.push(correo.value);
-        contraseñas.push(contraseña.value);
+        nombresR.push(nombre.value);
+        correosR.push(correo.value);
+        contraseñasR.push(contraseña.value);
+        contraseñas2R.push(contraseña2.value);
+        alert("IRegistro existoso");
+        limpiar();
     }
+}
+
+function limpiar() {
+    nombre.value = "";
+    correo.value = "";
+    contraseña.value = "";
+    contraseña2.value = "";
+}
+
+function validar_contacto() {
+    alert("Ingreso correcto");
 }
