dab53d3bd933405c9d35e0ed7d55980d2d1c61dc
AllanxSF <al.salinast@duocuc.cl>
2025-09-07 01:30:20 -0300

    validaciones

--- MijiBeauty-main/js/main.js
+++ MijiBeauty-main/js/main.js
@@ -1,17 +1,22 @@
 
 let nombresR = [];
 let correosR = [];
+let direcionR = [];
+let telefonoR = [];
 let contraseñasR = [];
 let contraseñas2R = [];
 
 let nombresC = [];
 let correosC = [];
+let telefonoC = [];
 let asuntoC = [];
 let descripcionC = [];
 
 function validar_registro() {
     let correo = document.getElementById("correo");
     let nombre = document.getElementById("nombre");
+    let direcion = document.getElementById("direcion");
+    let telefono = document.getElementById("telefono");
     let contraseña = document.getElementById("contraseña");
     let contraseña2 = document.getElementById("contraseña2");
 
@@ -33,24 +38,37 @@ function validar_registro() {
     } else {
         nombresR.push(nombre.value);
         correosR.push(correo.value);
+        direcionR.push(direcion.value);
+        telefonoR.push(telefono.value);
         contraseñasR.push(contraseña.value);
         contraseñas2R.push(contraseña2.value);
+
+        console.log("Nombres registrados:", nombresR);
+        console.log("Correos registrados:", correosR);
+        console.log("Direcciones registradas:", direcionR);
+        console.log("Telefonos registrados:", telefonoR);
+        console.log("Contraseñas registradas:", contraseñasR);
+        console.log("Confirmaciones registradas:", contraseñas2R);
         alert("IRegistro existoso");
-        limpiar();
+        limpiarR();
     }
 }
 
-function limpiar() {
+function limpiarR() {
     nombre.value = "";
     correo.value = "";
+    direcion.value = "";
+    telefono.value = "";
     contraseña.value = "";
     contraseña2.value = "";
+
 }
 
 function validar_contacto() {
 
     let correo = document.getElementById("correo");
     let nombre = document.getElementById("nombre");
+    let telefono = document.getElementById("telefono");
     let asunto = document.getElementById("asunto");
     let descripcion = document.getElementById("descripcion");
 
@@ -70,8 +88,25 @@ function validar_contacto() {
         nombresC.push(nombre.value);
         correosC.push(correo.value);
         asuntoC.push(asunto.value);
+        telefonoC.push(telefono.value);
         descripcionC.push(descripcion.value);
+        limpiarC();
         alert("Enviado con exito");
+
+        console.log("Nombres registrados:", nombresC);
+        console.log("Correos registrados:", correosC);
+        console.log("Telefonos registradas:", telefonoC);
+        console.log("Asuntos registrados:", asuntoC);
+        console.log("Descripcioines registradas:", descripcionC);
     }
-    
+
+}
+
+function limpiarC() {
+    nombre.value = "";
+    correo.value = "";
+    telefono.value = "";
+    asunto.value = "";
+    descripcion.value = "";
+
 }
